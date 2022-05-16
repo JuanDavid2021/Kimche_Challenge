0453998a2cd9282b246d6e45594f1ebb5ac8cf53
JuanDavid2021 <davidyk_15@hotmail.com>
2022-05-16 16:39:53 -0500

    style:styles were applied to the country card

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -6,12 +6,14 @@ import { Country } from './Country';
 export const CountriesList = () => {
 
  const {dataFilter} = useContext(CountriesContext) 
-
   return (
     <div>
         {dataFilter.length===0 ? <p>Por favor busca un pais </p> : dataFilter.map(countries=>(
+         
           <Country key={countries.code} {...countries}/>  
-        )) }
+        )) 
+         
+        }
     </div>
   )
 }
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,21 +1,31 @@
 import React from 'react'
+import "../styles/Country.css"
 
-export const Country = ({name, code}) => {
-
+export const Country = ({name, code, languages, continent,capital}) => {
+  
   function flagURL(code) {
     return `https://cdn.jsdelivr.net/npm/react-flagkit@1.0.2/img/SVG/${code}.svg`;
   } 
 
   return (
-    <div>
+    <div className='country'>
+     <div className='data'> 
      {
        name==="Bonaire"? <img src={`/assets/bonaire.png`} alt="Bonaire"/>
        :name==="Western Sahara"? <img src={`/assets/Western_Sahara.png`} /> : <img src={flagURL(code)} alt="Western Sahara" />
      }
       <h3>{name}</h3>
-      <button>
+      </div> 
+      <div className='countryInfo'>
+      <h6>Capital: {capital}</h6>
+      <h6>Languages: {languages.map(language=>language.name)}</h6>
+      <h6>Continent: {continent.name}</h6>
+      </div>
+      <div className='btn'>
+      <button >
         ver mas
       </button>
+      </div>
     </div>
   )
 }
