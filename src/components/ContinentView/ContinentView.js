83ee24f9108dcd6cb495b3a64d189bf9108c62c4
JuanDavid2021 <davidyk_15@hotmail.com>
2022-05-16 20:34:25 -0500

    fix:filter of continents and countries was fixed

--- src/components/ContinentView/ContinentView.js
+++ src/components/ContinentView/ContinentView.js
@@ -1,20 +1,50 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { CountriesContext } from '../context/CountriesContext';
-import { ContinentCard } from './ContinentCard';
+import { Country } from '../Country';
+import "../../styles/ContinentView.css"
 
 
 export const ContinentView = () => {
 
- const {dataFilter} = useContext(CountriesContext);
+  const {dataFilter} = useContext(CountriesContext)
+
+  const [inview, setInview] = useState(12);
+
+  let arrCountries = dataFilter?.slice(0, inview);
+
+  const showMoreItems = () => {
+    setInview((value) => value + 12);
+  };
+
+  const showLessItems = () => {
+    setInview((value) => value - 12);
+  };
+
 
   return (
-    <div>  
-    {dataFilter.map(countries=>(
-      <div>
-      <h1>{countries.continent.name}</h1>
-      <ContinentCard key={countries.code} {...countries}/>  
-      </div>
-    ))
-    }
+    <div className='continent'>  
+   {arrCountries.length===0 ? <p>Por favor busca un pais </p> : arrCountries.map(countries=>(
+
+   <div>
+   <h2>{countries.continent.name}</h2>
+   <Country key={countries.code} {...countries}/> 
+   </div>))}
+
+    {dataFilter.length <= arrCountries.length ? (
+       null) : (
+      <div className='more'>   
+      <button onClick={showMoreItems} className="boton">
+        More
+      </button>
+      </div> 
+       )}
+
+    {arrCountries.length > 13 && (
+      <div className='less'>
+        <button onClick={showLessItems} className="boton">
+          Less
+        </button>
+      </div>)}
     </div>
     )};
+
