695ddcaa809875dae57ed47a656af030561ca1ed
JuanDavid2021 <davidyk_15@hotmail.com>
2022-05-18 20:03:54 -0500

    fix:yarn is removed for deploy

--- src/client.js
+++ src/client.js
@@ -1,5 +1,6 @@
+import { ApolloClient } from "@apollo/react-hooks";
 import {InMemoryCache } from "apollo-boost";
-import { default as ApolloClient } from 'apollo-boost';
+
 
 export const client = new ApolloClient({
 uri: 'https://countries.trevorblades.com',
